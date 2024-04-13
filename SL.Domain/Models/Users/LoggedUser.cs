93cdcae99ba20e94447a83ee091ec4878509685c
Guilherme Souza <guilhermesouzae21@gmail.com>
2024-04-13 00:25:12 -0300

    chore: user id int to string switch

--- SL.Domain/Models/Users/LoggedUser.cs
+++ SL.Domain/Models/Users/LoggedUser.cs
@@ -2,7 +2,7 @@ namespace SL.Domain.Models.Users;
 
 public class LoggedUser
 {
-    public int Id { get; set; }
+    public string Id { get; set; }
     public string Name { get; set; }
     public string Email { get; set; }
 }
--- SL.WebApi/Middlewares/LoggedUserMiddleware.cs
+++ SL.WebApi/Middlewares/LoggedUserMiddleware.cs
@@ -9,9 +9,9 @@ public class LoggedUserMiddleware(RequestDelegate next)
     public async Task InvokeAsync(HttpContext context, LoggedUser loggedUser)
     {
         var user = context.User;
-        loggedUser.Id = int.TryParse(user.FindFirst("id")?.Value, out int userId) ? userId : 0;
-        loggedUser.Name = user.FindFirst("name")?.Value;
-        loggedUser.Email = user.FindFirst("email")?.Value;
+        loggedUser.Id = user?.FindFirst("id")?.Value;
+        loggedUser.Name = user?.FindFirst("name")?.Value;
+        loggedUser.Email = user?.FindFirst("email")?.Value;
 
         await _next(context);
     }
